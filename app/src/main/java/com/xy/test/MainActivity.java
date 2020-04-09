533a70540f725e566395ebe5230a4928b0b15a47
misteryuan <857951500@qq.com>
2020-04-09 09:09:11 +0800

    加入RecyclerView

--- app/src/main/java/com/xy/test/MainActivity.java
+++ app/src/main/java/com/xy/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.xy.test;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import android.os.Bundle;
@@ -14,31 +16,25 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private UserDatabase test;
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         test = Room.databaseBuilder(getApplicationContext(), UserDatabase.class, "test").build();
-        textView = findViewById(R.id.textView);
+
         final UserViewModel userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+
         findViewById(R.id.button_add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                new insertAsyncTask(test.userDao()).execute(new User("a"));
                 userViewModel.insert(new User("userDef"));
             }
         });
-        userViewModel.getUserLiveData().observe(this, new Observer<List<User>>() {
-            @Override
-            public void onChanged(List<User> users) {
-                String s = "";
-                for (User user : users) {
-                    s += "," + user.name;
-                }
-                textView.setText(String.valueOf(s.length()));
-            }
-        });
+
+        RecyclerView recyclerView = findViewById(R.id.user_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new UserListAdapter());
     }
 }
---
+++ app/src/main/java/com/xy/test/UserListAdapter.java
@@ -0,0 +1,37 @@
+package com.xy.test;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserViewHolder> {
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_item, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        holder.textView.setText("haha");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_view_user_name);
+        }
+    }
+}
